3a2f41dc90c6ba46417315109356a38c75f50fde
Maribel Obando Joven <maribel-obando.j@utp.edu.co>
2023-06-18 16:17:09 -0500

    ejercicio 7 completo

--- 05_EjerciciosCiclos/Ejercicio07.java
+++ 05_EjerciciosCiclos/Ejercicio07.java
@@ -11,19 +11,58 @@ public class Ejercicio07{
         System.out.print("Ingrese el tercer numero entero: ");
         int num3 = numeros.nextInt();
 
-        String multiplos1 = "";
-        String multiplos2 = "";
-        String multiplos3 = "";
+        int mcm = 0;
+        int pri = 2;
+        int resultado1;
+        int resultado2;
+        int resultado3;
+        int mult= 1;
+        int cont = 0;
+        
+        while(mcm == 0){
+            
+            if(num1 %  pri == 0 || num2 %  pri == 0 || num3 %  pri == 0){
+                do{
+                    if(num1 %  pri == 0){
+                        resultado1= num1/pri;
+                        num1 = resultado1;
+                    }
+                    if(num2 %  pri == 0){
+                        resultado2= num2/pri;
+                        num2 = resultado2;
+                    }
+                    if(num3 %  pri == 0){
+                        resultado3= num3/pri;
+                        num3 = resultado3;
+                    }
 
-        for( int i=0; i<=10; i++ ){
-			int resultado1 = num1 * i;
-            int resultado2 = num1 * i;
-            int resultado3 = num1 * i;
+                    mult= mult * pri;
+                }while(num1 %  pri == 0 || num2 %  pri == 0 || num3 %  pri == 0);
 
-            multiplos1 = multiplos1 + Integer.toString(resultado1)+ ", ";
-            multiplos2 = multiplos2 + Integer.toString(resultado2)+ ", ";
-            multiplos3 = multiplos3 + Integer.toString(resultado3)+ ", ";
-		}        
+                pri = pri + 1;
+
+                if(num1 == 1 & num2 == 1 & num3 == 1){
+                    mcm = mult;
+                    System.out.print("el MCM de esos numeros es: "+ mcm);
+                }
+            }else{
+                pri = pri + 1;
+            }
+
+            for( int i = 1; i <= 100; i++){
+			
+                if(pri % i == 0){
+                    cont = cont + 1;
+                    if(cont>2){
+                        pri = pri + 1;
+                        cont = 0;
+                        break;
+                    } 
+                }else{
+                    pri = pri;
+                }
+            }
+        }             
 
     }
 }
