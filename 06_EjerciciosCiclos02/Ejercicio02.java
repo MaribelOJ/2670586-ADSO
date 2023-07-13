239a725659c60d5cccb7a6b503dde4ca4235e415
Maribel Obando Joven <maribel-obando.j@utp.edu.co>
2023-07-13 12:12:30 -0500

    versión 2

--- 06_EjerciciosCiclos02/Ejercicio02.java
+++ 06_EjerciciosCiclos02/Ejercicio02.java
@@ -2,12 +2,54 @@ public class Ejercicio02{
     public static void main(String[] args){
         
         String wholenum = "";
+        int dia, mes, year, fecha;
+        int cont = 0;
 
-        for(int i = 1; i <= 8; i++){
+        while (cont < 1){
 
-            int numeros = (int) ( Math.random() * 9);
-            wholenum = wholenum + numeros;
-        }       
+            for(int i = 1; i <= 8; i++){
+
+                int numeros = (int) ( Math.random() * 9);
+                fecha = numeros;
+                wholenum = wholenum + fecha;
+            }
+            
+            dia = Integer.parseInt(wholenum.substring(0,2));
+            mes = Integer.parseInt(wholenum.substring(2,4));
+            year = Integer.parseInt(wholenum.substring(4,8));
+
+            if(dia > 31 || dia <= 00){
+                cont=0;
+                wholenum = "";
+            }else if (dia <=31 && mes <= 00){
+                cont = 0;
+                wholenum = "";
+            }else if(dia <= 31 && mes > 12){
+                cont = 0;
+                wholenum = "";
+            }else if(dia == 31 && mes <= 12){
+                if(mes == 01 | mes == 03 | mes == 05 | mes == 07 | mes == 8 | mes == 10 | mes == 12){
+                    cont = 1;
+                }else{
+                    cont = 0;
+                    wholenum = "";
+                }
+            }else if (dia <= 28 && mes <= 12){
+                cont = 1;
+            }else if (dia > 28 && mes <= 12 && mes != 02){
+                cont = 1;
+            }else if (dia == 29 && mes == 02){
+
+                if (year % 4 == 0 || year % 100 == 0 && year % 400 == 0){
+                    cont = 1;
+                }else{
+                    cont = 0;
+                    wholenum = "";
+                }
+            }else{
+                cont = 1;
+            }       
+        }        
 
         System.out.println("La fecha generada es: " + wholenum);
     }
