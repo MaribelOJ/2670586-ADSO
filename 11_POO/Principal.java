1d640cab8473375c0bea48f87cb1eba3ef3043aa
xFranco10 <andreyfranco109@gmail.com>
2023-10-06 17:00:36 -0500

    POO

---
+++ 11_POO/Principal.java
@@ -0,0 +1,18 @@
+public class Principal{
+    public static void main(String [] args){
+
+        //clases:Define nuevos tipos de datos.Una clase es una plantilla que se usará para crear objetos/variables.
+
+        //Atributos:variables globales que almacenan propiedades de la clase definida o una caracteristica.Datos propios.
+
+
+        //Metodos: Acciones que puede realizar un objeto perteneciente a la clase.Se representan como Funciones.
+
+        Perro perro1 = new Perro("Dobby", "Maribel", 108, "pomeranio", 2.50, 60);
+        perro1.comer(800);
+        perro1.imprimirDetalle();
+        
+        
+   
+    }
+}
